1edb8e6b954b825891b02efc7bf8400e7b671161
MyCodeBits <swapan.shridhar@hotmail.com>
2017-07-21 18:12:05 -0700

    Updated formating as per Google style guide.

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,15 +11,15 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+    /**
+    TODO:
+      * Calculate the RMSE here.
+    */
 }
 
-MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+MatrixXd Tools::CalculateJacobian(const VectorXd &x_state) {
+    /**
+    TODO:
+      * Calculate a Jacobian here.
+    */
 }
